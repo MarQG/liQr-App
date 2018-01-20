5f5e2bc78a0f17d05c8b5a80cc1f85e1126a45b9
Ferenc Gutierrez <ferenc.g87@gmail.com>
2018-01-19 19:13:12 -0600

    Merged melbranch and corrected controller object

--- controllers/drinksController.js
+++ controllers/drinksController.js
@@ -3,15 +3,15 @@
 module.exports = {
     drinks: (req, res) => {
         res.render('drinks/index');
-    }
+    },
     // newDrink
     newDrink: (req, res) => {
     	res.render('drinks/editdrink');
-    }
+    },
     // updateDrink
     updateDrink: (req, res) => {
     	res.render('drinks/newdrink');
-    }
+    },
     // showDrinks
     showDrinks: (req, res) => {
     	res.render('drinks/showdrink');
