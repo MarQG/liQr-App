5dadafa1c7b81cd7b7cd40bcffab4e4123ebb7d8
unknown <Collincavallo@gmail.com>
2018-01-20 16:34:58 -0600

    repulling backend

--- controllers/drinksController.js
+++ controllers/drinksController.js
@@ -7,7 +7,10 @@ module.exports = {
         //call db.drinks.findAll() and render the page while passing in the data
         // res.render( args1, args2)
         db.drinks.findAll({}).then((results) => {
-            console.log(results[0].drink_name);
+            // console.log(results[0].drink_name);
+            if(!results){
+                res.status(404).end();
+            }
             res.render('drinks/index', { drinks: results });
         });
        
