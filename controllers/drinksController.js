da95115a482b1863aed98aabcf7d97351330b915
Ferenc Gutierrez <ferenc.g87@gmail.com>
2018-01-20 13:25:00 -0600

    Adjusting drinks routes

--- controllers/drinksController.js
+++ controllers/drinksController.js
@@ -1,10 +1,14 @@
 // Requires
 // require the models as a db variable
+const db = require('../models');
 
 module.exports = {
     drinks: (req, res) => {
         //call db.drinks.findAll() and render the page while passing in the data
         // res.render( args1, args2)
+        db.drinks.findAll({}).then((results) => {
+            console.log(results[0]);
+        });
         res.render('drinks/index');
     },
     // newDrink
