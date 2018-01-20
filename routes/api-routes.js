2ffc5103e3ff067477f84d87d79f2048e08e3134
Melesa Martin <melesamartin@Melesa-Martins-MacBook-Pro.local>
2018-01-20 15:45:49 -0600

    added code to api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,4 +9,22 @@ const apiController = require('../controllers/apiContoller.js');
 // Exports
  module.exports = (app) => {
      app.get('/api/drinks', apiController.apiDrinks);
+
+     app.post('/api/drinks', apiController.apiDrinks);
+
+     app.put('/api/drinks', apiController.apiDrinks);
+
+     app.get('/api/drinks', apiController.apiDrinks);
+
+     app.delete('/api/comments', apiController.apiDrinks);
+
+     app.post('/api/comments', apiController.apiDrinks);
+
+     app.put('/api/comments', apiController.apiDrinks);
+
+     app.delete('/api/comments', apiController.apiDrinks);
+
+  	
  }
+
+ 
