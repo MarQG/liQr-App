d072edc3c14929d0339c6dfdd3f5ea2334ff1bf2
Ferenc Gutierrez <ferenc.g87@gmail.com>
2018-01-20 13:41:22 -0600

    Setup drinks route to pull all drinks

--- controllers/drinksController.js
+++ controllers/drinksController.js
@@ -7,9 +7,10 @@ module.exports = {
         //call db.drinks.findAll() and render the page while passing in the data
         // res.render( args1, args2)
         db.drinks.findAll({}).then((results) => {
-            console.log(results[0]);
+            console.log(results[0].drink_name);
+            res.render('drinks/index', { drinks: results });
         });
-        res.render('drinks/index');
+       
     },
     // newDrink
     newDrink: (req, res) => {
