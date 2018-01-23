312db8cd7990d09cd5050389c4739cd0c3f13205
Collin Cavallo <collincavallo@gmail.com>
2018-01-23 12:02:03 -0600

    commit before master pull

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,13 +8,33 @@ $('document').ready(function(){
     
     $('#test').on('click',function () {$('.ui.sidebar.inverted').sidebar('toggle')}) 
 
+    $('#loginForm').form({
+        onSuccess: function(e) {
+            // e.preventDefault();
+            console.log('sucess ran')
+            $.post('/login', {
+                email: $('#emailForm').val().trim(),
+                password: $('#passwordForm').val().trim()
+            }).then(function() {
+                console.log('form Submitted');
+            })
+        }
+    })
+
     $('#newCommentForm').form({
         fields: {
 
         },
-        onSuccess: function () {
-            $.get('/api/user_data').then(function (userid) {
-                console.log(userid + '*************************************************************************************************************************************************************************************');
+        onSuccess: function (e) {
+            var url_array = document.location.split('/');
+            var id = url_array[url_array.length - 1];
+            e.preventDefault()
+            $.get('/api/user_data').then(function (user) {
+                $.post('/api/comments', {
+                    userId: user.userId,
+                    comment: $('#commentText').val().trim(),
+                    drinkId: id
+                })
             })
         }
     })
