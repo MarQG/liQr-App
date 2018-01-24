ba64efbc5f4d44089aa04a377de4340f22bf17cf
Collin Cavallo <collincavallo@gmail.com>
2018-01-23 18:18:47 -0600

    commit before backend pull

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,4 @@
-$('document').ready(function(){
+$('document').ready(function () {
     var drinkId = $('#newCommentForm').attr('data-dataid');
     console.log(drinkId)
     console.log('main.js connected');
@@ -6,38 +6,36 @@ $('document').ready(function(){
     $('.img').hide()
     $('.img').transition('jiggle');
     $('.drinkName').transition('jiggle');
-    
-    
-    $('#test').on('click',function () {$('.ui.sidebar.inverted').sidebar('toggle')}) 
+
+
+    $('#test').on('click', function () {
+        $('.ui.sidebar.inverted').sidebar('toggle')
+    })
 
     $('#loginForm').form({
-        fields:{
+        fields: {
             email: {
-                rules:[
-                    {
-                        type: 'email',
-                        message: 'Not a valid email. Please enter a valid email address.'
-                    }
-                ]
-                
+                rules: [{
+                    type: 'email',
+                    message: 'Not a valid email. Please enter a valid email address.'
+                }]
+
             },
             password: {
-                rules:[
-                    {
-                        type: 'empty',
-                        message: 'Password cannot be empty.'
-                    }
-                ]
-                
+                rules: [{
+                    type: 'empty',
+                    message: 'Password cannot be empty.'
+                }]
+
             }
         },
-        onSuccess: function(e) {
+        onSuccess: function (e) {
             e.preventDefault();
             console.log('success ran');
             $.post('/login', {
                 email: $('#emailForm').val().trim(),
                 password: $('#passwordForm').val().trim()
-            }).then(function() {
+            }).then(function () {
                 window.location = "/drinks/";
                 console.log('form Submitted');
             });
@@ -63,25 +61,29 @@ $('document').ready(function(){
     });
     $('#upVote').on('click', function () {
         $.get('/api/user_data').then(function (user) {
-            $.get('/api/rating/', {
-                drinkId: drinkId,
-                userId: user.userId
-            }).then(function (response) {
+            $.get('/api/rating/' + drinkId + '/' + user.userId, {}).then(function (response) {
                 console.log(response)
                 // if response equals not found
-                if(response === 'not found') {
+                if (response === 'not found') {
                     //allow post rating
                     $.post('/api/ratings', {
                         rating: 1,
                         userId: userId,
                         drinkId: drinkId
                     })
-                }else{
-                    $.put('/api/ratings/' + response.id, {
-                        rating: !response.rating,
-                        userId: userId,
-                        drinkId: drinkId
-                    }).then(function (){location.reload()})
+                } else {
+                    $.ajax({
+                        url: '/api/ratings/' + response.id,
+                        type: 'PUT',
+                        data: {
+                            rating: !response.rating,
+                            userId: response.userId,
+                            drinkId: response.drinkId
+                        },
+                        success: function () {
+                            location.reload()
+                        }
+                    })
                 }
                 //allow edit
             })
@@ -89,7 +91,7 @@ $('document').ready(function(){
     })
     $('#downVote').on('click', function () {
         $.get('/api/user_data').then(function (user) {
-            $.get('/api/rating/', {
+            $.get('/api/rating/' + drinkId + '/' + user.userId, {
                 drinkId: drinkId,
                 userId: user.userId
             }).then(function (response) {
@@ -100,14 +102,21 @@ $('document').ready(function(){
                     $.post('/api/ratings', {
                         rating: 0,
                         userId: userId,
-                        drinkId: drinkId
+                        drinkId: drinkId,
                     })
                 } else {
-                    $.put('/api/ratings/' + response.id, {
-                        rating: !response.rating,
-                        userId: userId,
-                        drinkId: drinkId
-                    }).then(function () { location.reload() })
+                    $.ajax({
+                        url: '/api/ratings/' + response.id,
+                        type: 'PUT',
+                        data: {
+                            rating: !response.rating,
+                            userId: response.userId,
+                            drinkId: response.drinkId
+                        },
+                        success: function () {
+                            location.reload()
+                        }
+                    })
                 }
                 //allow edit
             })
@@ -118,22 +127,22 @@ $('document').ready(function(){
         fields: {
 
         },
-        onSuccess: function(e) {
-            $.post('/api/drinks', {  
-                    name: $('#drink_name_form').val().trim(),
-                    description: $('#drink_desc_form').val().trim(),
-                    imageLink: $('#drink_img_form').val().trim()               
-            }).then(function(){
+        onSuccess: function (e) {
+            $.post('/api/drinks', {
+                name: $('#drink_name_form').val().trim(),
+                description: $('#drink_desc_form').val().trim(),
+                imageLink: $('#drink_img_form').val().trim()
+            }).then(function () {
                 window.location.href('/drinks')
             })
         }
     })
 
-    $('#addComment').on('click',function() {
+    $('#addComment').on('click', function () {
         $('#commentForm').show()
         $('#addComment').hide();
 
     })
-    
+
 
 });
